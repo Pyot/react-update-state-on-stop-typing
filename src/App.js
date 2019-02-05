8124a32afd846d7ba149a290e1040635c2e81b3f
Piotr <piotrludwig@Codemalts-MacBook-Pro.local>
2019-02-05 16:15:14 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-class App extends Component {
+class ForHeavyData extends Component {
+  state = {
+    value: ''
+  }
+
+  timer = null
+
+  handleChange = (e) => {
+    clearTimeout(this.timer);
+    let value = e.target.value;
+    this.timer = setTimeout(() => { this.triggerChange(value) }, 2000);
+  }
+
+  triggerChange = (targetValue) => {
+    this.setState({ value: targetValue })
+  }
+
   render() {
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className={'container'}>
+        <div className={'row'}>
+          <div className={'col-md-6'}>
+            <div class="form-group">
+              <label for="exampleInputEmail1"></label>
+              <input
+                className={'form-control'}
+                placeholder={'Type text'}
+                defaultValue={this.state.value}
+                onChange={this.handleChange}
+                onKeyDown={this.handleKeyDown} />
+              <small className={"form-text text-muted"}>You'll see what you type after 2sec when you stop typing below.</small>
+            </div>
+          </div>
+        </div>
+        <div className={'row'}>
+          <div className={'col-md-6'}>
+            <p style={{'color': 'red'}}>
+              {this.state.value !== '' ? this.state.value : 'You will see updated state here'}
+            </p>
+          </div>
+        </div>
       </div>
-    );
+
+    )
   }
 }
 
-export default App;
+export default ForHeavyData
